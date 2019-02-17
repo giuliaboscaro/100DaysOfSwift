0e2b5ff5450f36b03fa81bb092621b2d36091838
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-02-16 23:52:37 -0200

    Day 16 | Project 1 | Part 1

---
+++ Log/Project1/Project1/ListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Giulia Boscaro on 16/02/19.
+//  Copyright © 2019 Giulia Boscaro. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "pictureCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pictureCell", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+
+}
+
