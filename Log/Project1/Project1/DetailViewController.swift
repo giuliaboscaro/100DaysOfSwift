690d2689b668065bad369c9a49eb15d445caf066
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-02-17 22:00:38 -0300

    Day 17 | Project 1 | Part 2

---
+++ Log/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Giulia Boscaro on 17/02/19.
+//  Copyright © 2019 Giulia Boscaro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    func setupView() {
+        view.backgroundColor = UIColor.white
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let pictureImage: UIImageView = {
+            let image = UIImageView()
+            image.translatesAutoresizingMaskIntoConstraints = false
+            if let imageName = selectedImage {
+                image.image = UIImage(named: imageName)
+            }
+            return image
+        }()
+        
+        view.addSubview(pictureImage)
+        
+        pictureImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pictureImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pictureImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pictureImage.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    
+    
+}
--- Log/Project1/Project1/ListViewController.swift
+++ Log/Project1/Project1/ListViewController.swift
@@ -15,6 +15,9 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -35,7 +38,15 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pictureCell", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.selectedImage = pictures[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
 
 
