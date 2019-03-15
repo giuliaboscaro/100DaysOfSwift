918de457136a87a5a009861e66b54e2cb06c37ba
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-03-14 23:31:58 -0300

    Day 42 | Project 10 | Part 1

---
+++ Log/Project10/Project10/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Giulia Boscaro on 14/03/19.
+//  Copyright © 2019 Giulia Boscaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .black
+        collectionView.register(PersonCell.self, forCellWithReuseIdentifier: "personCell")
+    }
+    
+    func setupView() {
+    
+    
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "personCell", for: indexPath) as? PersonCell else {
+            fatalError()
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 140, height: 180)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
+
+
