96734201e5f12bee8c5cd46c5a4bb9b156468b66
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-03-29 19:30:55 -0300

    Day 56 | Project 14 | Review and challenges

--- Log/Project14/Project14/GameScene.swift
+++ Log/Project14/Project14/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene {
     var slots = [WhackSlot]()
     
     var popupTime = 0.85
+    var numRounds = 0
     
     var score = 0 {
         didSet {
@@ -48,6 +49,31 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                score -= 5
+                
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                
+            } else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                whackSlot.hit()
+                score -= 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
         
     }
     
@@ -59,6 +85,28 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+            scoreLabel.text = "Your score was: \(score)"
+            scoreLabel.position = CGPoint(x: 512, y: 350)
+            scoreLabel.horizontalAlignmentMode = .center
+            scoreLabel.fontSize = 60
+            scoreLabel.zPosition = 1
+            addChild(scoreLabel)
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            return
+        }
+        
         popupTime *= 0.991
         
         slots.shuffle()
--- Log/Project14/Project14/WhackSlot.swift
+++ Log/Project14/Project14/WhackSlot.swift
@@ -35,6 +35,9 @@ class WhackSlot: SKNode {
     func show(hideTime: Double) {
         if isVisible { return }
         
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
@@ -57,5 +60,15 @@ class WhackSlot: SKNode {
         if !isVisible { return }
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
+    }
+    
+    func hit() {
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [weak self] in self?.isVisible = false }
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
     }
 }
