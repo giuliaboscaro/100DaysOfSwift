521a55163b0bd9dcf40d561f5bb1bfc3925be628
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-04-08 22:22:39 -0300

    Day 67 | Project 19 | Part 1

---
+++ Log/Project19/Extension/ActionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Giulia Boscaro on 08/04/19.
+//  Copyright © 2019 Giulia Boscaro. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {
+                    [weak self] (dict, error) in
+                    
+                }
+            }
+        }
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
---
+++ Log/Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Giulia Boscaro on 08/04/19.
+//  Copyright © 2019 Giulia Boscaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
